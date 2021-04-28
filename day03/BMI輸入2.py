38c7d9f07574ebc4bd5874a48eae6ac7d87cfe85
LE-T <vincentjava@yahoo.com.tw>
2021-04-28 19:35:20 +0800

    python

--- day03/BMI輸入2.py
+++ day03/BMI輸入2.py
@@ -3,3 +3,9 @@ h = float(input('請輸入身高:'))
 w = float(input('請輸入體重:'))
 bmi = w / math.pow(h/100, 2)
 print('BMI計算結果: %.2f' % bmi)
+if bmi > 23:
+    print("過胖")
+elif bmi <= 18:
+    print("過輕")
+else:
+    print("正常")
