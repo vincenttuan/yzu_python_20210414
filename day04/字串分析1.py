ae43ecf7ba778add270ed0d3427923c29e73edf8
LE-T <vincentjava@yahoo.com.tw>
2021-05-05 19:16:36 +0800

    python

--- day04/字串分析1.py
+++ day04/字串分析1.py
@@ -14,3 +14,9 @@ print('有幾個單字:', str.count(' ') + 1)
 # 是否都是英文字(a-zA-Z)
 # 小技巧: 先利用 replace 將中間的空白去除
 print('是否都是英文字:', str.replace(' ', '').isalpha())
+print(str[2])
+print(str[0:3]) # 取出0~3位置的字串(不含3)
+path = 'C:\temp\nba\score.py'
+print('路徑位置:', path)
+path = r'C:\temp\nba\score.py'
+print('路徑位置:', path)
