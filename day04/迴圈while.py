c30c18f2f9ef1affa9283b6747f1f385458a9fc3
LE-T <vincentjava@yahoo.com.tw>
2021-05-05 19:32:07 +0800

    python

--- day04/迴圈while.py
+++ day04/迴圈while.py
@@ -3,6 +3,7 @@ while True:
     n = r.randint(1, 100)
     if n % 3 == 0:
         print(n)
+        continue # 繼續執行下個迴圈(此次剩下的程式碼不會執行)
     if n == 50:
         print('離開迴圈, n=', n)
-        break
+        break # 強制跳離迴圈
---
+++ day04/迴圈while2.py
@@ -0,0 +1,6 @@
+# 得到偶數的次數
+import random as r
+n = 0
+while n % 2 == 0:
+    n = r.randint(1, 9)
+    print(n)
