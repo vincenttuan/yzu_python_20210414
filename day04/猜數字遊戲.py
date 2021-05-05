58964e7e75f8f421cac4f04b1aa89b0f09545508
LE-T <vincentjava@yahoo.com.tw>
2021-05-05 19:46:25 +0800

    python

---
+++ day04/猜數字遊戲.py
@@ -0,0 +1,14 @@
+import random as r
+ans = 47
+min = 0
+max = 100
+
+while True:
+    guess = int(input('請輸入 %d ~ %d : ' % (min, max)))
+    if guess > ans:
+        max = guess
+    elif guess < ans:
+        min = guess
+    else:
+        print('答對了')
+        break;
