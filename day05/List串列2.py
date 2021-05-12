c590830bee0d1943fbeebdf0f7b76ae3e484f62a
LE-T <vincentjava@yahoo.com.tw>
2021-05-12 19:15:32 +0800

    python

--- day05/List串列2.py
+++ day05/List串列2.py
@@ -1,4 +1,4 @@
-scores = [100, -10, 90, 80, 999]
+scores = [100, -10, -20, 90, -80, 999]
 print(scores)
 # error_score = scores.pop(1)
 # print(error_score, scores)
@@ -6,4 +6,14 @@ print(scores)
 # print(error_score, scores)
 
 # 利用 pop() 將不合法的分數挑出
+error_scores = []
+for s in scores:
+    if s < 0 or s > 100:
+        error_scores.append(s)
 
+print(scores)
+print(error_scores)
+for e in error_scores:
+    scores.pop(scores.index(e))
+
+print(scores)
