49e840b540b8389f7a728f3afe53ca9cc2729197
LE-T <vincentjava@yahoo.com.tw>
2021-05-12 19:28:25 +0800

    python

---
+++ day05/List串列3.py
@@ -0,0 +1,11 @@
+# append, insert, extend
+scores = [20, 30, 10]
+scores.append(50)
+print(scores)
+scores.insert(0, 10)
+print(scores)
+scores.append([70, 75, 79])
+print(scores[5][0])
+print(scores)
+scores.extend([80, 82])
+print(scores)
