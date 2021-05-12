735e0a9924220f1d35b091675855ae10924a0272
LE-T <vincentjava@yahoo.com.tw>
2021-05-12 21:17:13 +0800

    python

---
+++ day05/DictAndList2.py
@@ -0,0 +1,4 @@
+e1 = {'name': 'John', 'salary': 50000, 'program': ['HTML', 'JS']}
+e2 = {'name': 'Mary', 'salary': 60000, 'program': ['HTML', 'Python']}
+e3 = {'name': 'Bob', 'salary': 70000, 'program': ['Java', 'Python', 'C#']}
+# 請求出會 python 的員工人名, 並將放入到 names = [] 中
