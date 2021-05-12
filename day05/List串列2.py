504a24f4cce24016710e0c98c55ddb8af9211ed7
LE-T <vincentjava@yahoo.com.tw>
2021-05-12 19:03:44 +0800

    python

---
+++ day05/List串列2.py
@@ -0,0 +1,9 @@
+scores = [100, -10, 90, 80, 999]
+print(scores)
+# error_score = scores.pop(1)
+# print(error_score, scores)
+# error_score = scores.pop(3)
+# print(error_score, scores)
+
+# 利用 pop() 將不合法的分數挑出
+
