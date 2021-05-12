c46fdc188245aaea93fb0c76a9c237ac871397db
LE-T <vincentjava@yahoo.com.tw>
2021-05-12 20:57:37 +0800

    python

---
+++ day05/Dict字典串列.py
@@ -0,0 +1,12 @@
+# dict 是 key:value 的組合數組
+salary = {'John': 50000, 'Mary': 60000}
+print(salary)
+print('John 的薪資:', salary['John'])
+for key in salary:
+    print("%s 的薪資 %d" % (key, salary[key]))
+
+# 新增元素
+salary.setdefault('Bob', 70000)
+print(salary)
+
+# 求薪資總和
