1c6ea49acecd586496432536d9bd8debed17cdd1
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-02 19:59:29 +0800

    python

--- day06/DefDemo6.py
+++ day06/DefDemo6.py
@@ -11,9 +11,19 @@ def operator(func, x):
     else:
         return None
 
+def operator2(func1, func2, x):
+    if x > 0:
+        return func1(x) * func2(x)
+    else:
+        return None
+
 x = 1
 x = operator(add, x)
 print(x)
 
 x = operator(sub, x)
+print(x)
+
+x = 10
+x = operator2(add, sub, x)
 print(x)
