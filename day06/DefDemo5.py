4ae5c0d2d055cbf6f5326eedb2c1d03570025410
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-02 19:20:58 +0800

    python

--- day06/DefDemo5.py
+++ day06/DefDemo5.py
@@ -1,5 +1,6 @@
 x = 0
 y = 0
+z = 0
 
 def changeX(n):
     x = n  # x 是 changeX 的區域變數
@@ -10,6 +11,10 @@ def changeY(n):
     y = n
     print('changeY() 的 global y=', y)
 
+def changeZ(n):
+    z = pow(n, 2)
+    return z
+
 print('main() 的 x=', x)
 changeX(100)
 print('main() 的 x=', x)
@@ -17,3 +22,7 @@ print('main() 的 x=', x)
 print('main() 的 y=', y)
 changeY(100)
 print('main() 的 y=', y)
+
+print('main() 的 z=', z)
+z = changeZ(100)
+print('main() 的 z=', z)
