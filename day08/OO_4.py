1559ae1612002e0f91210d5e1f4b8c75d0a25939
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-16 20:27:39 +0800

    python

---
+++ day08/OO_4.py
@@ -0,0 +1,24 @@
+class Father:
+    amount = 0
+
+    def __init__(self, amount) -> None:
+        self.amount = amount
+
+    def __str__(self) -> str:
+        return "amount: " + str(self.amount)
+
+
+class Son(Father):
+    age = 0
+
+    def __init__(self, amount, age) -> None:
+        super().__init__(amount)
+        self.age = age
+
+    def __str__(self) -> str:
+        return super().__str__() + " age: " + str(self.age)
+
+
+if __name__ == '__main__':
+    son = Son(5000, 18)
+    print(son)
