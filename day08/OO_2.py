51c69da1fa7b7e3f299ccfccf7c06e0f0999a1fb
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-16 19:48:49 +0800

    python

--- day08/OO_2.py
+++ day08/OO_2.py
@@ -11,10 +11,16 @@ class Account:
     def addBalance(self, amount):  # 存款
         if(amount > 0):
             self.__balance = self.__balance + amount
+            return True
+        else:
+            return False
 
     def subBalance(self, amount):  # 提款
         if (amount > 0 and amount <= self.__balance):
             self.__balance = self.__balance - amount
+            return True
+        else:
+            return False
 
     def getBalance(self, password):  # 查詢餘額
         if(self.__password == password):
@@ -25,7 +31,9 @@ class Account:
 
     # 轉帳
     def transfer(self, acct, amount):
-        pass
+        status = self.subBalance(amount) # 提出
+        if(status):  # if(status == True)
+            acct.addBalance(amount) # 存入
 
     def __str__(self):
         return self.name + " 有 $" + str(self.__balance)
@@ -39,8 +47,14 @@ if __name__ == '__main__':
     # acc1.addBalance(200)
     # print(acc1)
 
-    acc2 = Account('Anita', 5000, '1234')
-    print('Balance $', acc2.getBalance('1234'))
-    acc2.subBalance(700)
-    print('Balance $', acc2.getBalance('1234'))
-    print(acc2)
+    # acc2 = Account('Anita', 5000, '1234')
+    # print('Balance $', acc2.getBalance('1234'))
+    # acc2.subBalance(700)
+    # print('Balance $', acc2.getBalance('1234'))
+    # print(acc2)
+
+    a = Account('Anita', 5000, '1234')
+    b = Account('Bob', 1000, '5678')
+    print(a, b)
+    a.transfer(b, 1500)
+    print(a, b)
