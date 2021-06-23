ee6c0246daf75c4064e1124ff52334f1e90dd568
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-23 20:24:32 +0800

    python

--- day09/SelectLottosLab.py
+++ day09/SelectLottosLab.py
@@ -1,2 +1,13 @@
 # 查出 n1 + n2 + .. + n5 = 60
 # 有哪幾筆 ?
+import sqlite3
+
+sql = 'select * from Lotto where n1+n2+n3+n4+n5 = 60'
+
+conn = sqlite3.connect('demo.db')
+cursor = conn.cursor()
+cursor.execute(sql)
+rows = cursor.fetchall()
+print(len(rows), rows)
+
+conn.close()
