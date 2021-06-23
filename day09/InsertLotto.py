c5e675fc052e175e928c360077d41b5811a7fe11
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-23 19:52:03 +0800

    python

--- day09/InsertLotto.py
+++ day09/InsertLotto.py
@@ -3,7 +3,8 @@ import sqlite3
 conn = sqlite3.connect('demo.db')
 cursor = conn.cursor()
 sql = '''
-        insert into Lotto(n1, n2, n3, n4, n5) values(1, 3, 5, 7, 9)
+        insert into Lotto(n1, n2, n3, n4, n5, ts) 
+        values(1, 3, 5, 7, 9, datetime(CURRENT_TIMESTAMP,'localtime'))
       '''
 
 cursor.execute(sql)
