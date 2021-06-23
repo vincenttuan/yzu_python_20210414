3f0c312eb1a77269d9b17cde9edcb08a9b19bfbc
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-23 19:34:50 +0800

    python

---
+++ day09/SelectLottos.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+conn = sqlite3.connect('demo.db')
+cursor = conn.cursor()
+
+# 查詢 sql
+sql = 'select id, n1, n2, n3, n4, n5, ts from Lotto'
+cursor.execute(sql)
+rows = cursor.fetchall()
+#print(rows)
+for r in rows:
+    print('{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(r[0], r[1], r[2], r[3], r[4], r[5], r[6]))
+
+conn.close()
