6dffcd7b51002d5406258220389ddc1b7e1849a5
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-23 21:28:43 +0800

    python

--- day09/DrawChart.py
+++ day09/DrawChart.py
@@ -9,6 +9,7 @@ print(df)
 # 繪圖
 ma = df['price'].rolling(window=2).mean()  # window=2 二點計算一個平均
 print(ma)
+plt.plot(df.id.values, df.price.values, 'r.') # 點紅點
 plt.plot(df['id'], df['price']) # 繪製折線圖
 plt.plot(df['id'], ma) # 繪製移動平均線
 # 圖例
