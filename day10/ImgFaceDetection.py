aa41d775a86ce3a772ac194d1d66c79b107e041c
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-30 19:51:18 +0800

    python

--- day10/ImgFaceDetection.py
+++ day10/ImgFaceDetection.py
@@ -2,6 +2,7 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier('./xml/haarcascade_frontalface_alt.xml')
 smile_cascade = cv2.CascadeClassifier('./xml/haarcascade_smile.xml')
+eye_cascade = cv2.CascadeClassifier('./xml/haarcascade_eye.xml')
 
 img = cv2.imread('./image/test.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -26,6 +27,17 @@ for (x, y, w, h) in faces:
 
     roi_gray = gray[y:y + h, x:x + w]
     roi_img = img[y:y + h, x:x + w]
+
+    eyes = eye_cascade.detectMultiScale(
+        roi_gray,  # 待檢測圖片，一般來說設定成灰度圖像可以加快檢測速度
+        scaleFactor=1.1,  # 檢測粒度。若粒度增加會加速檢測速度，但會影響準確率
+        minNeighbors=5,  # 每個目標至少要檢測到幾次以上，才被認定是真數據
+        minSize=(30, 30),  # 數據搜尋的最小尺寸
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    print("eyes:", len(eyes), eyes)
+    for (ex, ey, ew, eh) in eyes:
+        cv2.rectangle(roi_img, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 
     smile = smile_cascade.detectMultiScale(
         roi_gray,  # 待檢測圖片，一般來說設定成灰度圖像可以加快檢測速度
