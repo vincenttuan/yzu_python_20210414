34972407c223e7a412f2e68b7964a9600bbdb73e
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-30 20:33:04 +0800

    python

--- day10/MyFaceDetection.py
+++ day10/MyFaceDetection.py
@@ -19,8 +19,23 @@ while True:
 
     # 人臉偵測並畫紅框 ------------------------
 
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,  # 檢測粒度
+        minNeighbors=10,  # 每個目標至少偵測到幾次以上才被認定
+        minSize=(30, 30),  # 搜尋的最小尺寸
+        flags=cv2.CASCADE_SCALE_IMAGE  # 針對影像
+    )
 
+    print("faces: ", len(faces), faces)
+
+    for (x, y, w, h) in faces:
+        # 繪文字 putText(來源, 文字, 左下座標, 字型, 字型大小, 文字顏色, 文字線條寬度)
+        cv2.putText(img, 'Vincent', (x, y - 10), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+        # 畫框
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
     # 顯示在 ui 上
     cv2.imshow('My face', img)
